195edaf2f6ba2ada2e846d0506dbfe3435dbd01c
Cxmrykk <140925712+camerikk@users.noreply.github.com>
2023-12-21 16:46:25 +1100

    Update example

--- corolla-tcu-dev-interface/src/main.rs
+++ corolla-tcu-dev-interface/src/main.rs
@@ -6,6 +6,7 @@ use std::cell::RefCell;
 use std::rc::Rc;
 use std::time::Instant;
 
+#[derive(Clone)]
 struct GraphFunction {
     func: Rc<dyn Fn(f64) -> f64>,
     label: String,
@@ -38,25 +39,31 @@ fn create_window(app: &Application) -> ApplicationWindow {
         .default_height(600)
         .build();
 
-    let drawing_area = DrawingArea::new();
-    window.set_child(Some(&drawing_area));
-
     let functions = create_functions();
     let graph_values = create_graph_values(&functions);
     let start_time = Rc::new(Instant::now());
 
-    set_timeout(&start_time, &graph_values, &drawing_area, &functions);
-    set_draw_func(&start_time, &graph_values, &drawing_area, &functions);
+    let vbox = gtk4::Box::new(gtk4::Orientation::Vertical, 0);
+    window.set_child(Some(&vbox));
 
+    for i in 0..functions.len() {
+        let drawing_area = DrawingArea::new();
+        drawing_area.set_vexpand(true);
+        drawing_area.set_hexpand(true);
+        vbox.append(&drawing_area);
+        set_timeout(&start_time, &graph_values[i], &drawing_area, &functions[i]);
+        set_draw_func(&start_time, &graph_values[i], &drawing_area, &functions[i]);
+    }
+    
     window
 }
 
-fn create_functions() -> Rc<Vec<GraphFunction>> {
-    Rc::new(vec![
+fn create_functions() -> Vec<GraphFunction> {
+    vec![
         GraphFunction { func: Rc::new(f64::sin), label: "y = sin(x)".to_string(), color: RED },
         GraphFunction { func: Rc::new(f64::cos), label: "y = cos(x)".to_string(), color: BLUE },
         GraphFunction { func: Rc::new(f64::tan), label: "y = tan(x)".to_string(), color: GREEN },
-    ])
+    ]
 }
 
 fn create_graph_values(functions: &Vec<GraphFunction>) -> Vec<Rc<RefCell<Vec<(f64, f64)>>>> {
@@ -67,40 +74,40 @@ fn create_graph_values(functions: &Vec<GraphFunction>) -> Vec<Rc<RefCell<Vec<(f6
     graph_values
 }
 
-fn set_timeout(start_time: &Rc<Instant>, graph_values: &Vec<Rc<RefCell<Vec<(f64, f64)>>>>, drawing_area: &DrawingArea, functions: &Rc<Vec<GraphFunction>>) {
+fn set_timeout(start_time: &Rc<Instant>, graph_value: &Rc<RefCell<Vec<(f64, f64)>>>, drawing_area: &DrawingArea, function: &GraphFunction) {
     let start_time = start_time.clone();
-    let graph_values = graph_values.clone();
+    let graph_value = Rc::clone(graph_value);
     let drawing_area = drawing_area.clone();
-    let functions = Rc::clone(functions);
+    let function = function.clone();
 
     glib::timeout_add_local(std::time::Duration::from_millis(25), move || {
         let elapsed = start_time.elapsed().as_secs_f64();
-        for (i, graph_function) in functions.iter().enumerate() {
-            graph_values[i].borrow_mut().push((elapsed, (graph_function.func)(elapsed * std::f64::consts::PI)));
-        }
+        graph_value.borrow_mut().push((elapsed, (function.func)(elapsed * std::f64::consts::PI)));
         drawing_area.queue_draw();
         glib::ControlFlow::Continue
     });
 }
 
-fn set_draw_func(start_time: &Rc<Instant>, graph_values: &Vec<Rc<RefCell<Vec<(f64, f64)>>>>, drawing_area: &DrawingArea, functions: &Rc<Vec<GraphFunction>>) {
+fn set_draw_func(start_time: &Rc<Instant>, graph_value: &Rc<RefCell<Vec<(f64, f64)>>>, drawing_area: &DrawingArea, function: &GraphFunction) {
     let start_time = Rc::clone(start_time);
-    let graph_values = graph_values.clone();
-    let functions = Rc::clone(functions);
+    let graph_value = Rc::clone(graph_value);
+    let function = function.clone();
+
     drawing_area.set_draw_func(move |_, cr, width, height| {
-        render_graph(cr, width, height, start_time.clone(), &graph_values, &*functions, 40.0);
+        if width > 0 && height > 0 {
+            render_graph(cr, width, height, start_time.clone(), &*graph_value.borrow(), &function, 40.0);
+        }
     });
 }
 
-
-fn render_graph(cr: &cairo::Context, width: i32, height: i32, start_time: Rc<Instant>, graph_values: &Vec<Rc<RefCell<Vec<(f64, f64)>>>>, functions: &[GraphFunction], window_size: f64) {
+fn render_graph(cr: &cairo::Context, width: i32, height: i32, start_time: Rc<Instant>, graph_value: &[(f64, f64)], function: &GraphFunction, window_size: f64) {
     let elapsed = start_time.elapsed().as_secs_f64();
     let backend = plotters_cairo::CairoBackend::new(cr, (width as u32, height as u32)).unwrap();
     let root = backend.into_drawing_area();
     root.fill(&WHITE).unwrap();
 
     let mut chart = ChartBuilder::on(&root)
-        .caption("Line Graph", ("sans-serif", 50).into_font())
+        .caption(&function.label, ("sans-serif", 24).into_font())
         .margin(5)
         .x_label_area_size(30)
         .y_label_area_size(30)
@@ -109,16 +116,14 @@ fn render_graph(cr: &cairo::Context, width: i32, height: i32, start_time: Rc<Ins
 
     chart.configure_mesh().draw().unwrap();
 
-    for (i, graph_function) in functions.iter().enumerate() {
-        chart
-            .draw_series(LineSeries::new(
-                graph_values[i].borrow().iter().cloned(),
-                &graph_function.color,
-            ))
-            .unwrap()
-            .label(&graph_function.label)
-            .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &graph_function.color));
-    }
+    chart
+        .draw_series(LineSeries::new(
+            graph_value.iter().cloned(),
+            &function.color,
+        ))
+        .unwrap()
+        .label(&function.label)
+        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &function.color));
 
     chart.configure_series_labels().draw().unwrap();
 }
