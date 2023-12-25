bbc8d83ae39623ed0854a4e0bd9aa9a21127d145
Cxmrykk <140925712+camerikk@users.noreply.github.com>
2023-12-26 10:57:05 +1100

    Line buffering hack

--- corolla-tcu-dev-firmware/src/main.rs
+++ corolla-tcu-dev-firmware/src/main.rs
@@ -7,6 +7,7 @@ use std::io::{Write, stdout};
 fn main() -> anyhow::Result<()> {
     esp_idf_hal::sys::link_patches();
 
+    // Assign GPIO pins
     let peripherals = Peripherals::take()?;
     let mut gpio40 = PinDriver::input(peripherals.pins.gpio40)?; // LPS
     let mut gpio41 = PinDriver::input(peripherals.pins.gpio41)?; // SS1
@@ -17,31 +18,33 @@ fn main() -> anyhow::Result<()> {
     let mut gpio46 = PinDriver::input(peripherals.pins.gpio46)?; // RPM
     //let mut gpio15 = PinDriver::input(peripherals.pins.gpio15)?; // TPS
 
-    // Set pull down mode for all GPIO
-    gpio40.set_pull(Pull::Down)?;
-    gpio41.set_pull(Pull::Down)?;
-    gpio42.set_pull(Pull::Down)?;
-    gpio43.set_pull(Pull::Down)?;
-    gpio44.set_pull(Pull::Down)?;
-    gpio45.set_pull(Pull::Down)?;
-    gpio46.set_pull(Pull::Down)?;
-    //gpio15.set_pull(Pull::Down)?;
+    // Set mode for GPIO pins
+    gpio40.set_pull(Pull::Floating)?;
+    gpio41.set_pull(Pull::Floating)?;
+    gpio42.set_pull(Pull::Floating)?;
+    gpio43.set_pull(Pull::Floating)?;
+    gpio44.set_pull(Pull::Floating)?;
+    gpio45.set_pull(Pull::Floating)?;
+    gpio46.set_pull(Pull::Floating)?;
+    //gpio15.set_pull(Pull::Floating)?;
+
+    let mut stdout = stdout();
+    let mut buffer: [u8; 2] = [0x00, 0x0A]; // newline hack
 
     loop {
-        let buffer: [u8; 1] = [
-            0x00
-            | (gpio40.is_high() as u8)
-            | (gpio41.is_high() as u8) << 1
-            | (gpio42.is_high() as u8) << 2
-            | (gpio43.is_high() as u8) << 3
-            | (gpio44.is_high() as u8) << 4
-            | (gpio44.is_high() as u8) << 5
-            | (gpio46.is_high() as u8) << 6
-            //| (gpio15.is_high() as u8) << 7
-        ];
+        buffer[0] = 0x00
+        | (gpio40.is_high() as u8)
+        | (gpio41.is_high() as u8) << 1
+        | (gpio42.is_high() as u8) << 2
+        | (gpio43.is_high() as u8) << 3
+        | (gpio44.is_high() as u8) << 4
+        | (gpio45.is_high() as u8) << 5
+        | (gpio46.is_high() as u8) << 6
+        //| (gpio15.is_high() as u8) << 7
+        ;
 
-        stdout().write_all(&buffer)?;
-        
+        stdout.write_all(&buffer)?;
+        stdout.flush()?;
         FreeRtos::delay_ms(10);
     }
 }
